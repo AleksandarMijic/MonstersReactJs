7f67963d9fb66fa185d69da6e4f092525c3b80a5
AleksandarMijic <a.mijic90@hotmail.com>
2020-08-26 13:14:57 -0800

    Updates

---
+++ precache-manifest.6b5f9d88c2a13aa6e979c80be9f6816d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88232f4ac6996655f7451fe1faabd257",
+    "url": "/MonstersReactJs/index.html"
+  },
+  {
+    "revision": "f8635109778cd44af045",
+    "url": "/MonstersReactJs/static/css/main.34b2ae00.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonstersReactJs/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersReactJs/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8635109778cd44af045",
+    "url": "/MonstersReactJs/static/js/main.2864d549.chunk.js"
+  },
+  {
+    "revision": "ad16b397372b4e531621",
+    "url": "/MonstersReactJs/static/js/runtime-main.26092503.js"
+  }
+]);
